fb2851b0e7bd363a65216c3521e98aeeea8c64e4
palens22 <apc.dpc.palens@gmail.com>
2024-07-08 19:28:38 +0200

    Dibujar a distancia funcionando.

--- Panels Interactions/Assets/HandPainter.cs
+++ Panels Interactions/Assets/HandPainter.cs
@@ -30,13 +30,13 @@ public class HandPainter : MonoBehaviour
 
     private Vector3 scale;
 
-    private RaycastHit _touch;
+    private RaycastHit _touch, hit;
     private Whiteboard _whiteboard;
-    private Vector2 _touchPos,_lastTouchPos;
-    private bool _touchedLastFrame, activated;
-    private Quaternion _lastTouchRot;
+    private Vector2 _touchPos,_lastTouchPos, _lasthitPos, hitPos;
+    private bool _touchedLastFrame, activated, hitLastFrame;
+    private Quaternion _lastTouchRot, _lasthitRot;
     private InputAction drawAction;
-    public float rayLength = 5f; // Length of the ray
+    public float rayLength = 15f; // Length of the ray
 
     private LineRenderer _lineRenderer;
 
@@ -154,29 +154,29 @@ public class HandPainter : MonoBehaviour
         if (drawAction.ReadValue<float>() > 0.1f)
         {
             // Perform the raycast to paint from a distance
-            if (Physics.Raycast(_tip.position, transform.TransformDirection(Vector3.up), out _touch, rayLength, whiteboardMask))
+            if (Physics.Raycast(_tip.position, transform.TransformDirection(Vector3.up), out hit, rayLength, whiteboardMask))
             {
-                Pintar();
+                PintarDistancia();
                 Debug.Log("Button A pressed");
             }
         }
-        //else
-        //{
-        //    _whiteboard = null;
-        //    _touchedLastFrame = false;
-        //}
+        else
+        {
+            _whiteboard = null;
+            hitLastFrame = false;
+        }
     }
     private void Pintar()
     {
         if (_touch.transform.CompareTag("Whiteboard"))
         {
             _colors = Enumerable.Repeat(_renderer.material.color, tip_bulk * tip_bulk).ToArray();
-            _tipHeight = _tip.localScale.y;
+            
             if (_whiteboard == null)
             {
                 _whiteboard = _touch.transform.GetComponent<Whiteboard>();
             }
-            _touchPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
+            hitPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
 
             var x = (int)(_touchPos.x * _whiteboard.textureSize.x - (tip_bulk / 2));
             var y = (int)(_touchPos.y * _whiteboard.textureSize.y - (tip_bulk / 2));
@@ -186,7 +186,7 @@ public class HandPainter : MonoBehaviour
             if (_touchedLastFrame)
             {
                 
-               
+              
                 _whiteboard.texture.SetPixels(x, y, tip_bulk, tip_bulk, _colors);
 
                 for (float f = 0.01f; f < 1.00f; f += 0.01f)
@@ -211,5 +211,52 @@ public class HandPainter : MonoBehaviour
         }
          _whiteboard = null;
         _touchedLastFrame = false;
+    }
+    private void PintarDistancia()
+    {
+        if (hit.transform.CompareTag("Whiteboard"))
+        {
+            Debug.Log("Entra 1");
+            _colors = Enumerable.Repeat(_renderer.material.color, tip_bulk * tip_bulk).ToArray();
+            if (_whiteboard == null)
+            {
+                _whiteboard = hit.transform.GetComponent<Whiteboard>();
+            }
+            hitPos = new Vector2(hit.textureCoord.x, hit.textureCoord.y);
+            Debug.Log(hit.textureCoord.x);
+            Debug.Log(hit.textureCoord.y);
+            var x = (int)(hitPos.x * _whiteboard.textureSize.x - (tip_bulk / 2));
+            var y = (int)(hitPos.y * _whiteboard.textureSize.y - (tip_bulk / 2));
+
+            if (y < 0 || y > _whiteboard.textureSize.y || x < 0 || x > _whiteboard.textureSize.x) return;
+               
+            if (hitLastFrame)
+            {
+                
+               Debug.Log("Entra 2");
+                _whiteboard.texture.SetPixels(x, y, tip_bulk, tip_bulk, _colors);
+
+                for (float f = 0.01f; f < 1.00f; f += 0.01f)
+                {
+                    var lerpX = (int)Mathf.Lerp(_lasthitPos.x, x, f);
+                    var lerpY = (int)Mathf.Lerp(_lasthitPos.y, y, f);
+                    _whiteboard.texture.SetPixels(lerpX, lerpY, tip_bulk, tip_bulk, _colors);
+                }
+
+                transform.rotation = _lasthitRot;
+
+                _whiteboard.texture.Apply();
+            }
+
+            _lasthitPos = new Vector2(x, y);
+            _lasthitRot = transform.rotation;
+            hitLastFrame = true;
+
+            return;
+
+
+        }
+         _whiteboard = null;
+        hitLastFrame = false;
     }
 }
